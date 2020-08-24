067145e30d975d99a60d8c860d0324ed2db43482
Harrison Hua <harrisonhhua@gmail.com>
2020-08-24 17:33:48 -0500

    review controller and validation added to model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,20 @@
 class ReviewsController < ApplicationController
-
-    def new
-
+    def create
+        @genre = Genre.find(params[:genre_id])
+        @place = Place.find(params[:place_id])
+        @review = Review.new(review_params)
+        @review.place = @place
+        @review.save
+        if @review.save
+          redirect_to user_path(@review.place)
+        else
+          redirect_to user_path(@review.place)
+        end
     end
 
-    def create
-        
+    private
+    
+    def review_params
+        params.require(:review).permit(:rating, :content)
     end
 end
