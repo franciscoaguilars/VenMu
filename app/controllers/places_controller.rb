b8832ed591106fee749254213267d772c438866b
Francisco Aguilar <fiaguilars@gmail.com>
2020-08-24 17:33:44 -0500

    Basic geocode/route

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
+    geocoded_by :address
+    after_validation :geocode, if: :will_save_change_to_address?
     skip_before_action :authenticate_user!, only: [ :index, :show ]
+
+    
     def index
 
     end
