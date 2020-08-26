e0031a05e275de96a657e9377f0f7de7bc29ec4e
Jair Hernandez <jairhdzgaytan@gmail.com>
2020-08-26 10:53:10 -0500

    slide and pundit fixes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,78 +1,79 @@
 class PlacesController < ApplicationController
-    skip_before_action :authenticate_user!, only: [ :index, :show ]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
 
-    def index
-        if params[:query]
-            @query = params[:query]
-            sql_query = " \
-              name ILIKE :query \
-              OR top_genre ILIKE :query \
-            "
-            @places = Place.select("goats.*").where(sql_query, query: "%#{params[:query]}%")
+  def index
+      if params[:query]
+          @query = params[:query]
+          sql_query = " \
+            name ILIKE :query \
+            OR top_genre ILIKE :query \
+          "
+          @places = Place.select("goats.*").where(sql_query, query: "%#{params[:query]}%")
 
-            @geocodedPlaces = Goat.select("goats.*").where(sql_query, query: "%#{params[:query]}%").geocoded
+          @geocodedPlaces = Goat.select("goats.*").where(sql_query, query: "%#{params[:query]}%").geocoded
 
-            @markers = display_markers(@geocodedPlaces)
+          @markers = display_markers(@geocodedPlaces)
 
-        else
-            @places = Place.all
+      else
+          @places = policy_scope(Place)
 
-            @geocodedPlaces = Place.geocoded
+          @geocodedPlaces = Place.geocoded
 
-            @markers = display_markers(@geocodedPlaces)
-        end
-    end
+          @markers = display_markers(@geocodedPlaces)
+      end
+  end
 
-    def show
-        @place = Place.find(params[:id])
-    end
+  def show
+    @place = Place.find(params[:id])
+    authorize @place
+  end
 
-    def new
-        @place = Place.new
-    end
+  def new
+    @place = Place.new
+    authorize @place
+  end
 
-    def create
-        @place = Place.new(strong_params)
-        @place.user = current_user
+  def create
+      @place = Place.new(strong_params)
 
-        if @place.save
-            redirect_to place_path(@place)
-        else
-            render :new
-        end
-    end
+      if @place.save
+          redirect_to place_path(@place)
+      else
+          render :new
+      end
+  end
 
-    def edit
-        @place = Place.find(params[:id])
-    end
+  def edit
+      @place = Place.find(params[:id])
+  end
 
-    def update
-        @place = Place.find(params[:id])
-        @place.update(strong_params)
+  def update
+      @place = Place.find(params[:id])
+      @place.update(strong_params)
 
-        redirect_to place_path(@place)
-    end
+      redirect_to place_path(@place)
+  end
 
-    def destroy
-        @place = Place.find(params[:id])
-        @place.destroy
+  def destroy
+      @place = Place.find(params[:id])
+      @place.destroy
 
-        redirect_to places_path
-    end
+      redirect_to places_path
+  end
 
-    private
-    def strong_params
-        params.require(:place).permit(:name, :top_genre, :category, :address, :url, :description, :phone_number, photos: [])
-    end
+  private
+  def strong_params
+      params.require(:place).permit(:name, :top_genre, :category, :address, :url, :description, :phone_number, photos: [])
+  end
 
-    def display_markers(geocoded)
-        geocoded.map do |place|
-            {
-              lat: place.latitude,
-              lng: place.longitude,
-              infoWindow: render_to_string(partial: "info_window", locals: { place: place })
-            }
-        end
-    end
+  def display_markers(geocoded)
+      geocoded.map do |place|
+          {
+            lat: place.latitude,
+            lng: place.longitude,
+            infoWindow: render_to_string(partial: "info_window", locals: { place: place })
+          }
+      end
+  end
 end
